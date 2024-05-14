afff35467ed5e347cb4ae29e9d18077533808199
Brahim Maaqoul <wacakoule@gmail.com>
2024-05-14 08:49:16 +0100

    starting...

---
+++ module-00/ex00/program.java
@@ -0,0 +1,25 @@
+public class program {
+    public static void main(String[] args){
+        int num = 479985;
+        if (num < 100000 || num > 999999)
+        {
+            System.out.println("enter a number of 6 digits!");
+            return;
+        }
+        
+        int res1 = num / 100000;
+        num -= res1 * 100000;
+        int res2 = num / 10000;
+        num -= res2 * 10000;
+        int res3 = num / 1000;
+        num -= res3 * 1000;
+        int res4 = num / 100;
+        num -= res4 * 100;
+        int res5 = num / 10;
+        num -= res5 * 10;
+        int sum = num + res1 + res2 + res3 + res4 + res5;
+        System.out.println(sum);
+
+        // System.out.println(num);
+    }
+    }
