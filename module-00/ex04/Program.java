ad4ab790fa68c7cada6b94063e44e3f69d8d79ee
Brahim Maaqoul <wacakoule@gmail.com>
2024-11-25 20:48:01 +0100

    idk wut m doing

--- module-00/ex04/Program.java
+++ module-00/ex04/Program.java
@@ -13,7 +13,12 @@ public class Program {
 		}
 		return table;
 	}
-
+    // private  static String sort(String str){
+    //     for (int i = 0; i < str.length(); i++)
+    //     {
+            
+    //     }
+    // }
 	
 
 
@@ -23,14 +28,34 @@ public class Program {
 			// String str = in.next();
 			String str = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSSSSSSSSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDWEWWKFKKDKDSKAKLSLDKSKALLLLLLLLLLRTRTETWTWWWWWWWWWWOOOOOOO42";
 			int[] count = counter(str);
+            
             char[] a = str.toCharArray();
-            System.out.println(count['A']);
-			for (int i = 0; i < 10; i++) {
-                if ()
+            // System.out.println(count['D']);
+            int[] table = new int[10];
+            int j = 0;
+			for (int i = 0; i < str.length() - 1; i++)
+            {
+                if (count[a[i]] != count[a[i+1]] && count[a[i]] != 0 && j < 10)
                 {
-                    
+                    table[j] = count[a[i]];
+                    count[a[i]] = 0;
+                    j++;
                 }
 			}
+            int tmp = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = i; k < 10; k++)
+                {
+                    if (table[k] > table[i])
+                    {
+                        tmp = table[k];
+                        table[k] = table[i];
+                        table[i] = tmp;
+                    }
+                }
+                System.out.println(table[i]);
+            }
 
 		} catch (Exception e) {
 			System.err.println(e);
